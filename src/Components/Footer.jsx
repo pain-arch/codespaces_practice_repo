f355d8f48e17c36e3922869e9e93ced789373723
Md Shykat <himulol123@gmail.com>
2024-04-06 16:53:33 +0000

    Update social media links in Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -61,23 +61,23 @@ const Footer = (props) => {
         }}
       >
         {email && (
-          <a href={`mailto: shykat.workspace@gmail.com`}>
+          <a href={`mailto:${email}`}>
             <img src={envelopeIcon} alt="email" className="socialIcon" />
           </a>
         )}
         {devDotTo && (
-          <a href={`https://dev.to/pains_arch}`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://dev.to/${devDotTo}`} target="_blank" rel="noopener noreferrer">
             <img src={devDotToIcon} alt="Dev.to" className="socialIcon" />
           </a>
         )}
         {gitHub && (
-          <a href={`https://github.com/pain-arch`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
             <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
           </a>
         )}
         {instagram && (
           <a
-            href={`https://www.instagram.com/rip_arceneaux/`}
+            href={`https://www.instagram.com/${instagram}`}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -86,7 +86,7 @@ const Footer = (props) => {
         )}
         {linkedIn && (
           <a
-            href={`https://www.linkedin.com/in/md-shykat/`}
+            href={`https://www.linkedin.com/in/${linkedIn}`}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -94,18 +94,18 @@ const Footer = (props) => {
           </a>
         )}
         {medium && (
-          <a href={`https://medium.com/@Pains_arch`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://medium.com/@${medium}`} target="_blank" rel="noopener noreferrer">
             <img src={mediumIcon} alt="Medium" className="socialIcon" />
           </a>
         )}
         {twitter && (
-          <a href={`https://twitter.com/EarShykat`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noopener noreferrer">
             <img src={twitterIcon} alt="Twitter" className="socialIcon" />
           </a>
         )}
         {youTube && (
           <a
-            href={`https://www.youtube.com/c/@shykathimu`}
+            href={`https://www.youtube.com/c/${youTube}`}
             target="_blank"
             rel="noopener noreferrer"
           >
